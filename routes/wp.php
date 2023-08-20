c8041a42c81cf9cbcef725e97291931198f0d611
Filip Pacurar <filip@pacurar.net>
2023-08-21 01:59:06 +0300

    A very helpful 404 page that lists any tried action before 404 fallback

--- routes/wp.php
+++ routes/wp.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\Wp\Archive;
 use App\Http\Controllers\Wp\Search;
 use LaraWelP\Foundation\Support\Facades\WpRoute;
 
-WpRoute::home([Home::class, 'blog']);
-WpRoute::page([Page::class, 'index']);
-WpRoute::post('post', [Single::class, 'index']);
+//WpRoute::home([Home::class, 'blog']);
+//WpRoute::page([Page::class, 'index']);
+//WpRoute::post('post', [Single::class, 'index']);
 
 WpRoute::notFound([\App\Http\HandleNotFound::class, 'handle404']);
 WpRoute::addRoute('/_404', [\App\Http\HandleNotFound::class, 'handle404Terminate'], ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']);
